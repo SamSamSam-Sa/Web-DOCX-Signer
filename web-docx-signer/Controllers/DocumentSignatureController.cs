f14daf3d0f932e67e6a503237a4c9003d9e9e9be
Stasy <stasysamdance@gmail.com>
2020-08-07 17:12:40 +0300

    Web Api + swagger

---
+++ web-docx-signer/Controllers/DocumentSignatureController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace web_docx_signer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentSignatureController : ControllerBase
+    {
+        IWebHostEnvironment _appEnvironment;
+
+        //public DocumentSignatureController(IWebHostEnvironment appEnvironment)
+        //{
+        //    _appEnvironment = appEnvironment;
+        //}
+
+        //[HttpPost]
+        //public IEnumerable<FileModel> GetSignatureDocument(IFormFile uploadedFile)
+        //{
+        //}
+        //public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
+        //{
+        //    long size = files.Sum(f => f.Length);
+
+        //    foreach (var formFile in files)
+        //    {
+        //        if (formFile.Length > 0)
+        //        {
+        //            var filePath = Path.GetTempFileName();
+
+        //            using (var stream = System.IO.File.Create(filePath))
+        //            {
+        //                await formFile.CopyToAsync(stream);
+        //            }
+        //        }
+        //    }
+
+        //    // Process uploaded files
+        //    // Don't rely on or trust the FileName property without validation.
+
+        //    return Ok(new { count = files.Count, size });
+        //}
+        //[HttpPost]
+        //public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        //{
+        //    foreach (var uploadedFile in uploads)
+        //    {
+        //        // путь к папке Files
+        //        string path = "/Files/" + uploadedFile.FileName;
+        //        // сохраняем файл в папку Files в каталоге wwwroot
+        //        using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+        //        {
+        //            await uploadedFile.CopyToAsync(fileStream);
+        //        }
+        //        FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+        //        _context.Files.Add(file);
+        //    }
+        //    _context.SaveChanges();
+
+        //    return Ok;
+        //}
+    }
+}
---
+++ web-docx-signer/Models/FileModel.cs
@@ -0,0 +1,9 @@
+namespace web_docx_signer
+{
+    public class FileModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+}
